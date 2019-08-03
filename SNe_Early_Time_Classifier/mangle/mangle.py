204112c7db3e5ec77be9dba29a0d2c2e493bde0e
benggarza <bggarza@ucsc.edu>
2019-08-02 19:10:04 -0700

    Changed deprecated xrange to range

--- SNe_Early_Time_Classifier/mangle/mangle.py
+++ SNe_Early_Time_Classifier/mangle/mangle.py
@@ -784,7 +784,7 @@ class lightcurve_fit_george:
 		nwalkers = 5
 		init = np.array([0, 0, 0, t0, 40, -5])
 		ndim = len(init)
-		p0 = [np.array(init) + 1e-8 * np.random.randn(ndim) for i in xrange(nwalkers)]
+		p0 = [np.array(init) + 1e-8 * np.random.randn(ndim) for i in range(nwalkers)]
 		sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, args=(time, flux, fluxerr))
 
 		print("Running burn-in...")
